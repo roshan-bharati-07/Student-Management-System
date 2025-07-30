d6fccae35fcadcd6397b7fa780b4d89e4e534f0f
roshan-bharati-07 <bharati.roshan.098@gmail.com>
2025-07-30 11:16:12 +0545

    changes

--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -40,15 +40,14 @@ const router = express.Router();
 
 // Admin auth routes
 router.post('/register', adminRegister);     
-router.post('/login', adminLogin);           
 router.post('/resent-token', refreshAccessToken);   //  when access token expires, error is send and based on that new req is send without awaring client
+router.post('/login', adminLogin);           
 router.post('/logout', authMiddleware, logoutAdmin);   
 router.post('/forget-password', authMiddleware, forgetPassword);    
 router.post('/verify-code/:code', authMiddleware, verifyForgotPasswordCode);   
 router.patch('/set-new-password', authMiddleware, setNewPassword);       // don't need to send as params as I can get it from cookies
 router.patch('/reset-password', authMiddleware, reset_password);        
 router.get('/:username/profile', authMiddleware, getAdminDetails);      
-
 
 
 // holiday routes
